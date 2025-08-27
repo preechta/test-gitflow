d9ab0a359b760a6da386594f3a7ac2a26e9c2719
preechta <preechta@ais.co.th>
2025-08-27 17:42:08 +0700

    Continue Develop 7,8,

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ Console.WriteLine("Continue Develop 3");
 Console.WriteLine("Continue Develop 4");
 Console.WriteLine("Continue Develop 5");
 Console.WriteLine("Continue Develop 6");
+Console.WriteLine("Continue Develop 7");
+Console.WriteLine("Continue Develop 8");
 #endregion
 #region "Feature 2"
 Console.WriteLine("Add Feature 2");
