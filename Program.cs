4981044d5f8c565dd4f4ad67b10bfe4f8d5bf7a6
preechta <preechta@ais.co.th>
2025-08-27 17:33:10 +0700

    Continue Develop 3,4,5,6

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Init Project!");
 Console.WriteLine("Continue Develop 1");
 Console.WriteLine("Continue Develop 2");
+Console.WriteLine("Continue Develop 3");
+Console.WriteLine("Continue Develop 4");
+Console.WriteLine("Continue Develop 5");
+Console.WriteLine("Continue Develop 6");
