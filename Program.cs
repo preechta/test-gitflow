44356b7dab99cfde3078349a571d9e9991e9a819
preechta <preechta@ais.co.th>
2025-08-27 17:37:40 +0700

    Merge Feature 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Init Project!");
+#region "Develop"
 Console.WriteLine("Continue Develop 1");
 Console.WriteLine("Continue Develop 2");
 Console.WriteLine("Continue Develop 3");
 Console.WriteLine("Continue Develop 4");
 Console.WriteLine("Continue Develop 5");
 Console.WriteLine("Continue Develop 6");
+#endregion
+#region "Feature 2"
+Console.WriteLine("Add Feature 2");
+#endregion
